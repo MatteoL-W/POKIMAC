eb6e3d60fd5bcb3ee65095ae9ba1226c5029b459
Matteo L <matteo.ftt@gmail.com>
2021-12-03 21:48:08 +0100

    adding sdl for mingw

--- Makefile
+++ Makefile
@@ -29,7 +29,7 @@ endif
 
 #TODO: remplacez les build par $^
 render: main.o maths.o utils.o graphics.o
-	$(CC) -o bin/$@ build/main.o build/maths.o build/utils.o build/graphics.o
+	$(CC) -o bin/$@ build/main.o build/maths.o build/utils.o build/graphics.o -lmingw32 -lSDL2main -lSDL2
 
 main.o: $(SFOLDER)/main.cpp $(IFOLDER)/main.hpp
 	$(CC) -o build/$@ -c $< $(CFLAGS)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
-#include <iostream>
-using namespace std;
-#include "../include/main.hpp"
+#include <SDL2/SDL.h>
 
-int main() {
-    cout << "Test" << endl;
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    /* Initialisation simple */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+    {
+        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
+        return -1;
+    }
+
+    {
+        /* Création de la fenêtre */
+        SDL_Window* pWindow = NULL;
+        pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
+                                                                  SDL_WINDOWPOS_UNDEFINED,
+                                                                  640,
+                                                                  480,
+                                                                  SDL_WINDOW_SHOWN);
+
+        if( pWindow )
+        {
+            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voit la fenêtre */
+
+            SDL_DestroyWindow(pWindow);
+        }
+        else
+        {
+            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+        }
+    }
+
+    SDL_Quit();
+
+    return 0;
 }
